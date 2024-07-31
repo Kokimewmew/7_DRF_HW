f3b61506022b3df9806314bdfafe3b87eb401e5f
Artur <kokivirivoki@mail.ru>
2024-08-01 01:25:27 +0500

    HW_24_2

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from materials.models import Course, Lesson
 
@@ -9,8 +9,21 @@ class CourseSerializer(ModelSerializer):
         fields = '__all__'
 
 
+
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class CourseCountSerializer(ModelSerializer):
+    quantity_lesson = SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    def get_quantity_lesson(self, course):
+        return Lesson.objects.filter(course=course).count()
+
+    class Meta:
+        model = Course
+        fields = ('title', 'preview', 'description', 'quantity_lesson', 'lessons')
 
--- users/models.py
+++ users/models.py
@@ -1,21 +1,21 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from materials.models import Course, Lesson
+
+NULLABLE = {"blank": True, "null": True}
+
 
 class User(AbstractUser):
     username = None
 
-    email = models.EmailField(
-        unique=True, verbose_name='Почта', help_text='Укажите почту')
+    email = models.EmailField(unique=True, verbose_name='Почта', help_text='Укажите почту')
 
-    phone = models.CharField(
-        max_length=35, blank=True, null=True, verbose_name='Телефон', help_text='Укажите телефон')
+    phone = models.CharField(max_length=35, **NULLABLE, verbose_name='Телефон', help_text='Укажите телефон')
 
-    city = models.CharField(
-        max_length=50, blank=True, null=True, verbose_name='Город', help_text='Укажите город')
+    city = models.CharField(max_length=50, **NULLABLE, verbose_name='Город', help_text='Укажите город')
 
-    avatar = models.ImageField(
-        upload_to='users/avatars', blank=True, null=True, verbose_name='Аватар', help_text='Укажите аватар')
+    avatar = models.ImageField(upload_to='users/avatars', **NULLABLE, verbose_name='Аватар', help_text='Укажите аватар')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -23,3 +23,33 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+
+class Payments(models.Model):
+    method_choices = [
+        ('CASH', 'Наличными'),
+        ('TRANSFER', 'Перевод на счет'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Пользователь'),
+
+    date_payment = models.DateTimeField(auto_now_add=True, verbose_name='Дата оплаты'),
+
+    paid_course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Оплаченный курс', **NULLABLE),
+
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name='Оплаченный урок', **NULLABLE),
+
+    payment_sum = models.PositiveIntegerField(verbose_name='Cумма платежа')
+
+    payment_method = models.CharField(max_length=50, choices=method_choices, verbose_name='Способ оплаты')
+
+    session_id = models.CharField(max_length=255, verbose_name='ID сессии', **NULLABLE)
+
+    link = models.URLField(max_length=400, verbose_name='Ссылка на оплату', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
+
+    def __str__(self):
+        return f'{self.user} - {self.paid_course if self.paid_course else self.paid_lesson}'
